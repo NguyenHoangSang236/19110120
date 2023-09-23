bba0c174fe9c052c941b956b54d4bb499b520339
sang.nguyen <sang.nguyen>
2023-09-23 22:35:11 +0700

    update src code

---
+++ src/app/models/mygroupModels.js
@@ -0,0 +1,6 @@
+const mygroup = [
+    {id: "19110120", name:'Nguyen Hoang Sang'},
+    {id: "19110147", name:'Pham Hoang Phuc'},
+    {id: "19110128", name:'Nguyen Quoc Hoang'},
+]
+module.exports = { mygroup }
